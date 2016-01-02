e6066dbc1849f95c948d83e681e7a1f848c8c6d1
DanielHolst <danielholst2@gmail.com>
2016-01-02 15:23:55 +0100

    Add more objects to map and implement jump

--- leapShooter/Assets/scripts/movementScript.cs
+++ leapShooter/Assets/scripts/movementScript.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * Handles player movement
+ */
+
 public class movementScript : MonoBehaviour {
 
 	private Vector3 movementVec;
+	private Rigidbody rigidBody;
 
 	// Use this for initialization
 	void Start () {
 		movementVec = new Vector3(0f, 0f, 0f);
+		rigidBody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		//movement
 		movementVec.Set (0f, 0f, 0f);
 
 		if (Input.GetKey (KeyCode.A)) {
@@ -26,6 +33,11 @@ public class movementScript : MonoBehaviour {
 		}
 		if(Input.GetKey(KeyCode.D)) {
 			movementVec.Set (movementVec.x + 0.1f, movementVec.y, movementVec.z);
+		}
+
+		//jump
+		if (Input.GetKey (KeyCode.Space) && (transform.position.y <= 1f)) {
+			rigidBody.AddForce (Vector3.up*40f);
 		}
 
 		transform.position = transform.position + movementVec;
