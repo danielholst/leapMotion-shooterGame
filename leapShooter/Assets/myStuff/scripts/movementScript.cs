0ecef1c9a6ebe4d627e56669ef43412491d7d334
Daniel Holst <danielholst2@gmail.com>
2016-02-02 20:30:49 +0100

    structured assets

---
+++ leapShooter/Assets/myStuff/scripts/movementScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Handles player movement
+ */
+
+public class movementScript : MonoBehaviour {
+
+	private Vector3 movementVec;
+	private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		movementVec = new Vector3(0f, 0f, 0f);
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//movement
+		movementVec.Set (0f, 0f, 0f);
+
+		if (Input.GetKey (KeyCode.A)) {
+			movementVec.Set (movementVec.x - 0.1f, movementVec.y, movementVec.z);
+		}
+		if(Input.GetKey(KeyCode.W)) {
+			movementVec.Set (movementVec.x, movementVec.y, movementVec.z + 0.1f);
+		}
+		if(Input.GetKey(KeyCode.S)) {
+			movementVec.Set (movementVec.x, movementVec.y, movementVec.z - 0.1f);
+		}
+		if(Input.GetKey(KeyCode.D)) {
+			movementVec.Set (movementVec.x + 0.1f, movementVec.y, movementVec.z);
+		}
+
+		//jump
+		if (Input.GetKey (KeyCode.Space) && (transform.position.y <= 1f)) {
+			rigidBody.AddForce (Vector3.up*40f);
+		}
+
+		transform.position = transform.position + movementVec;
+	}
+}
---
+++ leapShooter/Assets/myStuff/scripts/myAimScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class myAimScript : MonoBehaviour {
+
+	public HandController controller;
+	public GameObject projectile;
+	private Leap.Vector fingerTipPos;
+	private Vector3 dir;
+	private Vector3 pos;
+	private GameObject instantiatedProjectile;
+	private bool shooting;
+
+
+	// Use this for initialization
+	void Start () {
+
+		shooting = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//get tip of finger position
+		fingerTipPos = controller.GetFrame().Hands[0].Fingers.Frontmost.TipPosition;
+
+		//convert to our world coordinates
+		pos = controller.transform.TransformPoint (fingerTipPos.ToUnityScaled (false));
+
+		//shoot projectile
+		if (Input.GetKeyUp ("space") && !shooting) {
+
+			//instantate new projectile
+			instantiatedProjectile = Instantiate (projectile, pos, new Quaternion(0f,0f,0f,0f) ) as GameObject;
+
+			//get finger direction
+			dir = controller.GetFrame ().Hands[0].Fingers.Frontmost.Direction.ToUnity ();
+
+			shooting = true;
+		}
+
+		//to move projectile
+		if (shooting) {
+			instantiatedProjectile.transform.position += dir;
+		}
+
+		//destroy object when out of reach
+		if (instantiatedProjectile != null && instantiatedProjectile.transform.position.z > 20) {
+			Destroy (instantiatedProjectile);
+			shooting = false;
+		}
+	}
+}
