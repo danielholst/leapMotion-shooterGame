e9a4c4bfe8edce59cd642c6f4ca5ba498b0315df
danielholst2@gmail.com <danielholst2@gmail.com>
2016-03-09 09:08:14 +0100

    add so you can quit game

---
+++ leapShooter/Assets/myStuff/scripts/endScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// To quit game when escape is pressed
+public class endScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			print ("Quit");
+			Application.Quit ();
+		}
+	}
+}
